de4789d6283351ea1fecc0f9134d4a4e6862c2a8
euhkim <euhkim@gmail.com>
2024-03-27 17:52:01 +0900

    Update : Stremlit App

--- dataframe.py
+++ dataframe.py
@@ -6,13 +6,13 @@ import seaborn as sns
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("gapminder.tsv", sep="\t")
+    df = pd.read_csv("gapminder.tsv", sep="\t") # 데이터프레임으로 반환
     return df
 
 def plot_matplotlib():
     st.title("**Bar Plot** with Seaborn")
-    df = load_data()
-    fig, ax = plt.subplots()
+    df = load_data() # 데이터 불러오기->데이터프레임 구조 변환
+    fig, ax = plt.subplots() # 시각화
     
     # Using Seaborn's barplot function
     sns.barplot(x=df['year'], y=df['lifeExp'], data=df, ax=ax)
@@ -20,8 +20,9 @@ def plot_matplotlib():
     # Labeling axes and title
     ax.set_xlabel("year")
     ax.set_ylabel("lifeExp")
+    ax.set_title("Year vs. lifeExp")
         
-    st.pyplot(fig)
+    st.pyplot(fig) # streamlit에 그리기
 
 def main():
     st.title("Data Display st.dataframe()")
